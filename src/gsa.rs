c566ab542146dba2d1a6e9832af761482c9b29aa
p3zhy <pejman.ghd@gmail.com>
2024-01-22 23:32:17 +0330

    Some typos fixed

--- src/gsa.rs
+++ src/gsa.rs
@@ -16,11 +16,11 @@ pub struct GSA {
     fix_sats_prn: [u16; MAX_PRNS_PER_MESSAGE],
     /// The actual number of PRNs in the array.
     prn_array_size: usize,
-    /// Position dilution of precision
+    /// Position dilusion of precision.
     pub pdop: f32,
-    /// Horizontal dilusion of presicion.
+    /// Horizontal dilusion of precision.
     pub hdop: f32,
-    /// Vertical dilution of precision.
+    /// Vertical dilusion of precision.
     pub vdop: f32,
 }
 
